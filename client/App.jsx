baffececd66b94fffb856cf6baa4f760ddb4838b
KP824 <112226540+KP824@users.noreply.github.com>
2023-04-07 16:02:26 -0700

    Created Login component. User inputs accessible to App component

--- client/App.jsx
+++ client/App.jsx
@@ -4,10 +4,31 @@ import './index.css';
 
 export default function App() {
   
+  const [userInfo, setUserInfo] = useState({
+    name: '',
+    phone: '',
+    password: '',
+  })
+
   return (
     <>
       <h1>Testing APP landing</h1>
-      <h1>Hello from kp branch</h1>
+      <br></br>
+      <div>
+        Testing input from Phone input:
+        {userInfo.phone}
+      </div>
+      <br></br>
+      <div>
+        Testing input from Password input:
+        {userInfo.password}
+      </div>
+      <div className='main-container'>
+        
+        <Login 
+          userInfo={userInfo} setUserInfo={setUserInfo}
+        />
+      </div>
     </>
   )
 };
--- client/components/Login.jsx
+++ client/components/Login.jsx
@@ -1,17 +1,70 @@
-// import React, { useState, useEffect } from 'react';
-// import axios from 'axios';
-// import { Navigate } from 'react-router-dom';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import { Navigate } from 'react-router-dom';
 
 
-// function Login() {
+function Login() {
 
+  const onChange = (event) => {
+    props.setUserInfo((prevState) => ({
+      ...prevState,
+      [event.target.name]: event.target.value,
+    }))
+  };
 
-//   };
+  const handleSubmit = (event) => {
+    event.preventDefault();
 
-//   return (
-//     <div>
-//     </div>
-//   )
-// };
+    const userLogin = {
+      phone: props.userInfo.phone,
+      password: props.userInfo.password
+    }
 
-// export default Login;
+    console.log(`userLogin: ${JSON.stringify(userLogin)}`);
+
+    // axios.get('/api/users/', userLogin).then((response) =>{
+
+    //   console.log(response.data)
+    // })
+
+  };
+
+  return (
+    <div className='login-container'>
+      <h3>Already a member? Please login</h3>
+      <br></br>
+      {/* <form className='form-container'> */}
+      <form onSubmit={handleSubmit}>
+        <div className='input-container'>
+          <p>Login Phone Number:</p>
+          <br></br>
+          <input 
+            type='text'
+            className='input-box'
+            id='phone'
+            name='phone'
+            value={props.userInfo.phone}
+            onChange={onChange}
+          />
+        </div>
+        <br></br>
+        <br></br>
+        <div className='input-container'>
+          <p>Login Password:</p>
+          <br></br>
+          <input 
+            type='text'
+            className='input-box'
+            id='password'
+            name='password'
+            value={props.userInfo.password}
+            onChange={onChange}
+          />
+        </div>
+        <button type='submit' className='styleMe' >Submit Login</button>
+      </form>
+    </div>
+  )
+};
+
+export default Login;
