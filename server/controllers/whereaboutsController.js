06b1d545a4e04d39f24009a3c84e2e81cf2beb80
Lucas Contreras <32287834+lucascontreras@users.noreply.github.com>
2023-04-08 02:56:43 -0400

    get all contacts of a user

--- server/controllers/whereaboutsController.js
+++ server/controllers/whereaboutsController.js
@@ -8,4 +8,17 @@ whereaboutsController.getUser = async (req, res, next) => {
     return next();
 };
 
+//get contacts of current user
+whereaboutsController.getContacts = async (req, res, next) => {
+    res.locals.contacts = await db.query(
+        `SELECT c.traveler_id, u.id as contact_id, u.name, u.phone_number
+        FROM users u
+        INNER JOIN contacts_join c
+        ON u.id = c.contact_id
+        WHERE c.traveler_id = $1`,
+        [req.params.id]
+    );
+    return next();
+};
+
 module.exports = whereaboutsController;
--- server/routes/api.js
+++ server/routes/api.js
@@ -9,6 +9,11 @@ router.get('/users/:id', whereaboutsController.getUser, (req, res) => {
     res.status(200).json(res.locals.user)
 });
 
+//get all contacts of current user
+router.get('/users/contacts/:id', whereaboutsController.getContacts, (req, res) => {
+  res.status(200).json(res.locals.contacts);
+});
+
 
 
 module.exports = router;
