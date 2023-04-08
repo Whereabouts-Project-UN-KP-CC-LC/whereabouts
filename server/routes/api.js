8d0fd2cbd1265e23079cbdc59967b8ad26bceea7
unatarajan <38731992+unatarajan@users.noreply.github.com>
2023-04-08 14:25:38 -0400

    Added login and register routes and middleware with bcrypt implemented for password hashing and comparison; minor tweaks to server, webpack, .gitignore

--- server/routes/api.js
+++ server/routes/api.js
@@ -2,13 +2,16 @@ const express = require('express');
 const router = express.Router();
 const whereaboutsController = require('../controllers/whereaboutsController');
 
+// LOGIN component routes
 
+router.post('/login', whereaboutsController.checkUserExists, (req, res) =>
+  res.status(200).json(res.locals.verifiedUser)
+);
 
-router.get('/users/:id', whereaboutsController.getUser, (req, res) => {
-  console.log(`res.locals.user: ${res.locals.user}`)
-    res.status(200).json(res.locals.user)
-});
+// REGISTER component routes
 
-
+router.post('/register', whereaboutsController.insertNewUser, (req, res) =>
+  res.status(200).json(res.locals.newUser)
+);
 
 module.exports = router;
